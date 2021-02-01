ac138c44ead83ebe89cccd6cbdb49a87fa3cdb26
CerteKim <Seelkadoow@live.com>
2021-02-01 12:42:55 +0800

    remove resolv.conf & systemlessly place xray binary

--- customize.sh
+++ customize.sh
@@ -8,11 +8,9 @@ SKIPUNZIP=1
 ui_print "- Prepare xray execute environment."
 mkdir -p /data/xray
 mkdir -p /data/xray/run
-mkdir -p /data/xray
+mkdir -p /data/xray/bin
 mkdir -p /data/xray/confs
 mkdir -p $MODPATH/scripts
-mkdir -p $MODPATH/system/bin
-mkdir -p $MODPATH/system/etc
 
 download_xray_zip="/data/xray/run/xray-core.zip"
 custom="/sdcard/Download/Xray-core.zip"
@@ -69,18 +67,15 @@ fi
 ui_print "- Install xray core $ARCH execute files"
 unzip -j -o "${download_xray_zip}" "geoip.dat" -d /data/xray >&2
 unzip -j -o "${download_xray_zip}" "geosite.dat" -d /data/xray >&2
-unzip -j -o "${download_xray_zip}" "xray" -d $MODPATH/system/bin >&2
+unzip -j -o "${download_xray_zip}" "xray" -d /data/xray/bin >&2
 unzip -j -o "${ZIPFILE}" 'xray/scripts/*' -d $MODPATH/scripts >&2
 unzip -j -o "${ZIPFILE}" 'service.sh' -d $MODPATH >&2
 unzip -j -o "${ZIPFILE}" 'uninstall.sh' -d $MODPATH >&2
 rm "${download_xray_zip}"
 # copy xray data and config
 ui_print "- Copy xray config and data files"
-[ -f /data/xray/resolv.conf ] || \
-unzip -j -o "${ZIPFILE}" "xray/etc/resolv.conf" -d /data/xray >&2
 [ -f /data/xray/confs/proxy.json ] || \
 unzip -j -o "${ZIPFILE}" "xray/etc/confs/*" -d /data/xray/confs >&2
-ln -s /data/xray/resolv.conf $MODPATH/system/etc/resolv.conf
 [ -f /data/xray/appid.list] || \
 echo ALL > /data/xray/appid.list
 # generate module.prop
@@ -89,14 +84,12 @@ rm -rf $MODPATH/module.prop
 touch $MODPATH/module.prop
 echo "id=xray" > $MODPATH/module.prop
 echo "name=Xray4Magisk" >> $MODPATH/module.prop
-echo -n "version=Module v1.2.2, Core " >> $MODPATH/module.prop
+echo -n "version=Module v1.2.3, Core " >> $MODPATH/module.prop
 echo ${latest_xray_version} >> $MODPATH/module.prop
-echo "versionCode=20210120" >> $MODPATH/module.prop
+echo "versionCode=20210201" >> $MODPATH/module.prop
 echo "author=CerteKim" >> $MODPATH/module.prop
 echo "description=xray core with service scripts for Android" >> $MODPATH/module.prop
 
-inet_uid="3003"
-net_raw_uid="3004"
 set_perm_recursive $MODPATH 0 0 0755 0644
 set_perm  $MODPATH/service.sh    0  0  0755
 set_perm  $MODPATH/uninstall.sh    0  0  0755
@@ -104,5 +97,5 @@ set_perm  $MODPATH/scripts/start.sh    0  0  0755
 set_perm  $MODPATH/scripts/xray.inotify    0  0  0755
 set_perm  $MODPATH/scripts/xray.service    0  0  0755
 set_perm  $MODPATH/scripts/xray.tproxy     0  0  0755
-set_perm  $MODPATH/system/bin/xray  0  ${inet_uid}  0755
-set_perm  /data/xray                0  ${inet_uid}  0755
+set_perm  $MODPATH/system/bin/xray  0  0  0755
+set_perm  /data/xray                0  0  0755
