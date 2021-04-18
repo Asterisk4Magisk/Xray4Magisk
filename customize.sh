14c7bf4a5821be28b41885dd31d034da10fca5b6
whalechoi <cj1369636717@gmail.com>
2021-04-19 01:24:26 +0800

    Migrate to "/data/adb/xray"

--- customize.sh
+++ customize.sh
@@ -1,16 +1,24 @@
-#!/sbin/sh
 #####################
 # xray Customization
 #####################
 SKIPUNZIP=1
 
+# migration of old configuration
+if [ -d "/data/xray" ]; then
+  ui_print "- Old configuration detected, migrating."
+  if [ -d "/data/adb/xray" ]; then
+    abort "Please remove \"/data/adb/xray\" first!"
+  fi
+  mv /data/xray/ /data/adb/xray/
+fi
+
 # prepare xray execute environment
 ui_print "- Prepare xray execute environment."
-mkdir -p /data/xray
-mkdir -p /data/xray/run
-mkdir -p /data/xray/bin
-mkdir -p /data/xray/confs
-mkdir -p $MODPATH/scripts
+mkdir -p /data/adb/xray
+mkdir -p /data/adb/xray/run
+mkdir -p /data/adb/xray/bin
+mkdir -p /data/adb/xray/confs
+mkdir -p /data/adb/scripts
 
 download_xray_zip="/data/xray/run/xray-core.zip"
 custom="/sdcard/Download/Xray-core.zip"
@@ -64,32 +72,32 @@ else
 fi
 
 # install scripts
-unzip -j -o "${ZIPFILE}" 'xray/scripts/*' -d $MODPATH/scripts >&2
+unzip -j -o "${ZIPFILE}" 'xray/scripts/*' -d /data/adb/scripts >&2
 unzip -j -o "${ZIPFILE}" 'service.sh' -d $MODPATH >&2
 unzip -j -o "${ZIPFILE}" 'uninstall.sh' -d $MODPATH >&2
-set_perm  $MODPATH/scripts/xray.service    0  0  0755
+set_perm  /data/adb/scripts/xray.service    0  0  0755
 
 # stop service
-$MODPATH/scripts/xray.service stop
+/data/adb/scripts/xray.service stop
 
 # install xray execute file
 ui_print "- Install xray core $ARCH execute files"
-unzip -j -o "${download_xray_zip}" "geoip.dat" -d /data/xray >&2
-unzip -j -o "${download_xray_zip}" "geosite.dat" -d /data/xray >&2
-unzip -j -o "${download_xray_zip}" "xray" -d /data/xray/bin >&2
+unzip -j -o "${download_xray_zip}" "geoip.dat" -d /data/adb/xray >&2
+unzip -j -o "${download_xray_zip}" "geosite.dat" -d /data/adb/xray >&2
+unzip -j -o "${download_xray_zip}" "xray" -d /data/adb/xray/bin >&2
 rm "${download_xray_zip}"
 
 # start service
-$MODPATH/scripts/xray.service start
+/data/adb/scripts/xray.service start
 
 # copy xray data and config
 ui_print "- Copy xray config and data files"
-[ -f /data/xray/confs/proxy.json ] || \
-unzip -j -o "${ZIPFILE}" "xray/etc/confs/*" -d /data/xray/confs >&2
-[ -f /data/xray/appid.list] || \
-echo ALL > /data/xray/appid.list
-[ -f /data/xray/ignore_out.list] || \
-touch /data/xray/ignore_out.list
+[ -f /data/adb/xray/confs/proxy.json ] || \
+unzip -j -o "${ZIPFILE}" "xray/etc/confs/*" -d /data/adb/xray/confs >&2
+[ -f /data/adb/xray/appid.list] || \
+echo ALL > /data/adb/xray/appid.list
+[ -f /data/adb/xray/ignore_out.list] || \
+touch /data/adb/xray/ignore_out.list
 
 # generate module.prop
 ui_print "- Generate module.prop"
@@ -106,10 +114,10 @@ echo "description=xray core with service scripts for Android" >> $MODPATH/module
 set_perm_recursive $MODPATH 0 0 0755 0644
 set_perm  $MODPATH/service.sh    0  0  0755
 set_perm  $MODPATH/uninstall.sh    0  0  0755
-set_perm  $MODPATH/scripts/start.sh    0  0  0755
-set_perm  $MODPATH/scripts/xray.inotify    0  0  0755
-set_perm  $MODPATH/scripts/xray.service    0  0  0755
-set_perm  $MODPATH/scripts/xray.tproxy     0  0  0755
-set_perm  /data/xray                       0  0  0755
-set_perm  /data/xray/bin                   0  0  0755
-set_perm  /data/xray/bin/xray              0  0  0755
+set_perm  /data/adb/scripts/start.sh    0  0  0755
+set_perm  /data/adb/scripts/xray.inotify    0  0  0755
+set_perm  /data/adb/scripts/xray.service    0  0  0755
+set_perm  /data/adb/scripts/xray.tproxy     0  0  0755
+set_perm  /data/adb/xray                       0  0  0755
+set_perm  /data/adb/xray/bin                   0  0  0755
+set_perm  /data/adb/xray/bin/xray              0  0  0755
--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,7 @@
 ##########################################################################################
 
 remove_xray_data_dir() {
-  rm -rf /data/xray
+  rm -rf /data/adb/xray
 }
 
 remove_xray_data_dir
