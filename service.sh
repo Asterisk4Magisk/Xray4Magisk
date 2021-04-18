06cc0172da86c939bd44c8de87b8bea05442856a
whalechoi <cj1369636717@gmail.com>
2021-04-19 01:24:26 +0800

    Migrate to "/data/adb/xray"

--- service.sh
+++ service.sh
@@ -6,5 +6,5 @@ MODDIR=${0%/*}
 until [ $(getprop sys.boot_completed) -eq 1 ] ; do
   sleep 5
 done
-${MODDIR}/scripts/start.sh
+/data/adb/scripts/start.sh
 )&
--- xray/scripts/start.sh
+++ xray/scripts/start.sh
@@ -1,11 +1,12 @@
 #!/system/bin/sh
 
-MODDIR=${0%/*}
+MODDIR=/data/adb/modules/xray4magisk
+SCRIPTS_DIR=/data/adb/xray/scripts
 
 start_proxy () {
-  ${MODDIR}/xray.service start &> /data/xray/run/service.log && \
-  if [ -f /data/xray/appid.list ] ; then
-    ${MODDIR}/xray.tproxy enable &>> /data/xray/run/service.log &
+  ${SCRIPTS_DIR}/xray.service start &> /data/adb/xray/run/service.log && \
+  if [ -f /data/adb/xray/appid.list ] ; then
+    ${MODDIR}/xray.tproxy enable &>> /data/adb/xray/run/service.log &
   fi
 }
 
@@ -13,7 +14,7 @@ until [ $(getprop sys.boot_completed) -eq 1 ]; do
   sleep 5
 done
 
-if [ ! -f /data/xray/manual ] ; then
+if [ ! -f /data/adb/xray/manual ] ; then
   start_proxy
-  inotifyd ${MODDIR}/xray.inotify ${MODDIR}/.. &>> /data/xray/run/service.log &
+  inotifyd ${SCRIPTS_DIR}/xray.inotify ${MODDIR} &>> /data/adb/xray/run/service.log &
 fi
