a29ae35206dfd8854e818e4b1ea7adb921f80e22
xqzr <34030394+xqzr@users.noreply.github.com>
2021-09-08 20:49:41 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,9 +4,7 @@
 #
 ##########################################################################################
 
-remove_xray_data_dir() {
-  rm -rf /data/adb/xray
-}
+#remove xray data dir
+rm -rf /data/adb/xray
 
-remove_xray_data_dir
 rm -rf /data/adb/service.d/xray4magisk_service.sh
