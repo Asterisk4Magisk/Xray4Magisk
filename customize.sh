da49304bd16b46549a3544a8fe66d11351c77c2e
CerteKim <18364439+CerteKim@users.noreply.github.com>
2022-02-21 11:36:14 +0800

    version 1.5.5

--- customize.sh
+++ customize.sh
@@ -140,9 +140,9 @@ rm -rf $MODPATH/module.prop
 touch $MODPATH/module.prop
 echo "id=xray4magisk" > $MODPATH/module.prop
 echo "name=Xray4Magisk" >> $MODPATH/module.prop
-echo -n "version=Module v1.5.3, Core " >> $MODPATH/module.prop
+echo -n "version=Module v1.5.5, Core " >> $MODPATH/module.prop
 echo ${latest_xray_version} >> $MODPATH/module.prop
-echo "versionCode=20211007" >> $MODPATH/module.prop
+echo "versionCode=20220221" >> $MODPATH/module.prop
 echo "author=CerteKim" >> $MODPATH/module.prop
 echo "description=xray core with service scripts for Android" >> $MODPATH/module.prop
 
