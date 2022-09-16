5560ed8c9791766d20a0bafe6f844564136100f2
CerteKim <18364439+CerteKim@users.noreply.github.com>
2022-09-16 20:27:18 +0800

    add support for sing-box

--- customize.sh
+++ customize.sh
@@ -20,23 +20,31 @@ fi
 
 config_file="${module_path}/confs/${config}"
 
+# get asset type and core type
 whichCustom() {
-  for i in $(ls /sdcard/Download); do
+  for i in $(ls -tr /sdcard/Download); do
     if [ $(echo $i | grep '(^Xray-)*(\.zip$)') ]; then
       asset="customDat"
-      return xray
+      core="xray"
+      download_file=$i
+      download_path="/sdcard/Download/${download_file}"
     elif [ $(echo $i | grep -nE '(^v2ray-)*(\.zip$)') ]; then
       asset="customDat"
-      return v2ray
+      core="v2ray"
+      download_file=$i
+      download_path="/sdcard/Download/${download_file}"
     elif [ $(echo $i | grep -nE '(^sing-box-)*(\.tar\.gz$)' ) ]; then
       asset="customDb"
-      return sing
+      core="sing-box"
+      download_file=$i
+      download_path="/sdcard/Download/${download_file}"
     elif
       abort
     fi
   done
 }
 
+# get version arg
 whichArch() {
   case "$1" in
     xray)
@@ -89,13 +97,26 @@ whichArch() {
   esac
 }
 
+# deploy assets
 asset() {
   case "${asset}" in
     loyalsoldier)
-
+      /data/adb/magisk/busybox wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O /sdcard/Download/geoip.dat >&2
+      if [ "$?" != "0" ] ; then
+        ui_print "Download err"
+        abort
+      fi
+      /data/adb/magisk/busybox wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O /sdcard/Download/geosite.dat >&2
+      if [ "$?" != "0" ] ; then
+        ui_print "Download err"
+        abort
+      fi
+      cp /sdcard/Download/geoip.db ${module_path}/assets
+        cp /sdcard/Download/geosite.db ${module_path}/assets
     ;;
     dat)
-
+      unzip -j -o "${download_path}" "geoip.dat" -d ${module_path}/assets >&2
+      unzip -j -o "${download_path}" "geosite.dat" -d ${module_path}/assets >&2
     ;;
     db)
       /data/adb/magisk/busybox wget https://github.com/SagerNet/sing-geoip/releases/latest/download/geoip.db -O /sdcard/Download/geoip.db >&2
@@ -130,6 +151,104 @@ asset() {
   esac
 }
 
-if [ "${core}" = "custom" ]; then
+whichCore() {
+  case "$core" in
+    custom)
+      whichCustom
+    ;;
+    xray)
+      download_link="https://github.com/XTLS/Xray-core/releases"
+      github_api="https://api.github.com/repos/XTLS/Xray-core/releases"
+      latest_version=`/data/adb/magisk/busybox wget -qO- ${github_api} | grep -m 1 "tag_name" | grep -o "[0-9.]*"`
+      whichArch
+      download_file=${version}
+      download_path="/sdcard/Download/${download_file}"
+      /data/adb/magisk/busybox wget "${download_link}/download/${latest_version}/${download_file}" -O "${download_path}" >&2
+      if [ "$?" != "0" ] ; then
+        ui_print "Download err"
+        abort
+      fi
+    ;;
+    v2ray)
+      download_link="https://github.com/v2fly/v2ray-core/releases"
+      github_api="https://api.github.com/repos/v2fly/v2ray-core/releases"
+      latest_version=`/data/adb/magisk/busybox wget -qO- ${github_api} | grep -m 1 "tag_name" | grep -o "[0-9.]*"`
+      whichArch
+      download_file=${version}
+      download_path="/sdcard/Download/${download_file}"
+      /data/adb/magisk/busybox wget "${download_link}/download/${latest_version}/${download_file}" -O "${download_path}" >&2
+      if [ "$?" != "0" ] ; then
+        ui_print "Download err"
+        abort
+      fi
+    ;;
+    sing-box)
+      download_link="https://github.com/SagerNet/sing-box/releases"
+      github_api="https://api.github.com/repos/SagerNet/sing-box/releases"
+      latest_version=`/data/adb/magisk/busybox wget -qO- ${github_api} | grep -m 1 "tag_name" | grep -o "[0-9.]*"`
+      whichArch
+      download_file="sing-box-${latest_version}-${version}.tar.gz"
+      download_path="/sdcard/Download/${download_file}"
+      /data/adb/magisk/busybox wget "${download_link}/download/v${latest_version}/${download_file}" -O "${download_path}" >&2
+      if [ "$?" != "0" ] ; then
+        ui_print "Download err"
+        abort
+      fi
+    ;;
+  esac
+}
 
-fi
+# deploy core
+core() {
+  case "${core}" in
+    xray)
+      unzip -j -o "${download_path}" "xray" -d ${module_path}/bin/ >&2
+    ;;
+    v2ray)
+      unzip -j -o "${download_path}" "v2ray" -d ${module_path}/bin/ >&2
+    ;;
+    sing-box)
+      tar --strip-components=1 -xvzf ${download_path} -C ${module_path}/bin/ >&2
+    ;;
+  esac
+}
+
+installModule() {
+  mkdir -p ${module_path}/bin
+  mkdir -p ${module_path}/run
+  mkdir -p ${module_path}/assets
+  mkdir -p ${module_path}/confs
+  mkdir -p ${module_path}/scripts
+  unzip -j -o "${ZIPFILE}" 'xray/scripts/*' -d ${module_path}/scripts >&2
+  if [ ! -d /data/adb/service.d ] ; then
+    mkdir -p /data/adb/service.d
+  fi
+  unzip -j -o "${ZIPFILE}" 'xray4magisk_service.sh' -d /data/adb/service.d >&2
+  unzip -j -o "${ZIPFILE}" 'uninstall.sh' -d $MODPATH >&2
+  set_perm  ${module_path}/scripts/xray.service    0  0  0755
+
+  [ -f ${module_path}/xray.config ] || \
+  unzip -j -o "${ZIPFILE}" "xray/etc/xray.config" -d ${module_path}>&2
+  [ -f ${config_file} ] || \
+  unzip -j -o "${ZIPFILE}" "xray/etc/confs/*" -d ${module_path}/confs >&2
+  [ -f ${module_path}/appid.list ] || \
+  echo "ALL" > ${module_path}/appid.list
+  [ -f ${module_path}/ignore_out.list ] || \
+  touch ${module_path}/ignore_out.list
+  [ -f ${module_path}/ap.list ] || \
+  [ "$(getprop ro.product.device)" = "rubens" ] && echo "ap+" > ${module_path}/ap.list || echo "wlan+" > ${module_path}/ap.list
+  [ -f ${module_path}/ipv6 ] || \
+  echo "enable" > ${module_path}/ipv6
+
+  set_perm_recursive $MODPATH 0 0 0755 0644
+  set_perm  /data/adb/service.d/xray4magisk_service.sh    0  0  0755
+  set_perm  $MODPATH/uninstall.sh                         0  0  0755
+  set_perm_recursive  ${module_path}/scripts              0  0  0755
+  set_perm  ${module_path}                                0  0  0755
+  set_perm_recursive  ${module_path}/bin                  0  0  0755
+
+  unzip -j -o "${ZIPFILE}" "module.prop" -d $MODPATH >&2
+  echo -n "version=Module v0.1, Core " >> $MODPATH/module.prop
+  echo "${core} ${latest_version}" >> $MODPATH/module.prop
+  echo "versionCode=20220916" >> $MODPATH/module.prop
+}
