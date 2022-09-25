e3f023db22e42cdf2943238e4255037c696deab3
Misaka-2020 <Tenngu2019@gmail.com>
2022-09-25 17:58:57 +0800

    some changes

--- README.md
+++ README.md
@@ -1,49 +1,72 @@
 English | [简体中文](README_zh_CN.md)
 
 # Xray4Magisk
-A fork of [V2ray for Android](https://github.com/Magisk-Modules-Repo/v2ray)
 
-This is a Xray module for Magisk, and includes binaries for arm, arm64, x86, x64.
+A fork from [V2ray for Android](https://github.com/Magisk-Modules-Repo/v2ray)
 
-
+This is a Magisk module for Xray/v2ray/sing-box, and includes binaries for arm, arm64, x86, x64.
 
 ## Disclaimer
+
 I'm not responsible for bricked devices, dead SD cards, or burning your SoC.
 
-Make sure you are not going to loopback traffic again and again. It might cause your phone reset, then bootloop.
+**Make sure your profile does not cause traffic to loop back, otherwise it may cause your phone to constantly reboot.**
 
 If you really don't know how to configure this module, you mignt need apps like v2rayNG, SagerNet(or AnXray) etc.
 
+## Manager APP
 
-
-# Manager APP
 [Xray4Magisk_Manager](https://github.com/whalechoi/Xray4Magisk_Manager)(WIP)
-
-
 
 ## Install
 
-You can download the release installer zip file and install it via the Magisk Manager App.
+Download the module zip from [Release](https://github.com/Asterisk4Magisk/Xray4Magisk/releases) and install it via [Magisk](https://github.com/topjohnwu/Magisk) or [Xray4Magisk_Manager](https://github.com/whalechoi/Xray4Magisk_Manager).
 
-### Download Xray binary: Auto
-NOTE: This module doesn't contain Xray-core binary. Instead, the installation process download the latest binary file from Xray's github releases.
+### Auto installation
 
-### Download Xray binary: Manual
-Download the Xray-core zip file and put it in `/sdcard/Download`.
+This module does not contain binaries such as [Xray-core](https://github.com/XTLS/Xray-core) or [sing-box](https://github.com/SagerNet/sing-box).
 
-NOTICE: It is important to check your device's CPU Architecture, and select the correct .zip file.  
-For example, for sdm855, we choose "Xray-android-arm64-v8a.zip".
+- Using Magisk to install.
 
+  - You can select the core and rule files to be installed with the volume keys
+  - You can customize the initial installation by placing the `xray.config` file in the `/sdcard` directory: (can be deleted after installation)
 
+  ```
+  core=
+  asset=
+  ```
+
+  `core` is the core file, valid value: `custom`/`xray`/`v2ray`/`sagernet`/`sing-box`
+
+  Value custom for custom, v2ray for [v2fly/v2ray-core](https://github.com/v2fly/v2ray-core), sagernet for [SagerNet/v2ray-core](https://github.com/SagerNet/v2ray-core)
+
+  `asset` is rules files with the following valid values: `loyalsoldier/dat/customDat/db/customDb`
+
+  loyalsoldier is [Loyalsoldier/v2ray-rules-dat](https://github.com/Loyalsoldier/v2ray-rules-dat), dat and db are the corresponding defaults, customDat and customDb are the corresponding customizations
+
+  **xray.config has a higher priority than the volume selection installation**
+
+  The upgrade installation defaults to the previous installation, or you can modify the `#Update` entry in `xray.config` in the `/data/adb/xray/` directory to determine the next installation, with the same valid values as above.
+
+### Manual installation (custom core)
+
+Download the core file for the corresponding architecture and package it in a zip archive, placing it in the `/sdcard/Download` directory along with the rules files (optional).
+
+The naming convention for core files is as follows: (\* indicates arbitrary characters)
+
+- xray ---> Xray-\*.zip
+- v2ray/sagernet ---> v2ray-\*.zip
+- sing-box ---> sing-box-\*.tar.gz
+
+**Note: Check the CPU architecture of your device and select the correct zip file.**
+
+For example, for sdm855 to download xray-core, download `Xray-android-arm64-v8a.zip`.
 
 ## Config
 
-- Xray config files are store in `/data/adb/xray/confs/*.json`.
-- proxy config is `/data/adb/xray/confs/proxy.json`.
-
-- Tips: Please notice that the default configuration has already set inbounds section to cooperate work with transparent proxy script. It is recommended that you only edit the `proxy.json` to your proxy server and edit file `/data/adb/xray/appid.list` to select which App to proxy. Edit file `ignore_out.list` can help you to ignore some OUTPUT interfaces.
-
-
+- The config file is stored in `/data/adb/xray/confs/*.json`
+- Tip: The default config already sets the inbounds section to work with transparent proxy scripts. It is recommended that you only edit the `outbounds` section to add your proxy server, for advanced configurations please refer to the appropriate official documentation, such as [Xray](https://xtls.github.io/) and [sing-box](https://sing-box.sagernet.org/)
+- The file `/data/adb/xray/appid.list` is used to select the APPs that require proxying. editing the `ignore_out.list` file allows you to ignore certain network exits, for example to enable not going through the proxy when connecting to WiFi.
 
 ## Usage
 
@@ -51,37 +74,43 @@ For example, for sdm855, we choose "Xray-android-arm64-v8a.zip".
 
 #### Manage service start / stop
 
-- Xray service is auto-run after system boot up by default.
-- You can use Magisk Manager App to manage it. Be patient to wait it take effect (about 3 second).
-- Check out [Manage service start / stop
-](https://github.com/Asterisk4Magisk/Xray4Magisk#manage-service-start--stop-1) and [Manage transparent proxy enable / disable](https://github.com/Asterisk4Magisk/Xray4Magisk#manage-transparent-proxy-enable--disable)
+**The following core services are collectively referred to as Xray**
 
+- Xray service is auto-run after system boot up by default.
+- You can use Magisk Manager App to manage it. Starting the service may take a few seconds, stopping it may take effect immediately.
 
 #### Select which App to proxy
 
-- Use [Xray4Magisk_Manager](https://github.com/whalechoi/Xray4Magisk_Manager)
-- Check out [Select which UID to proxy](https://github.com/Asterisk4Magisk/Xray4Magisk#select-which-uid-to-proxy)
+- You can use [Xray4Magisk_Manager](https://github.com/whalechoi/Xray4Magisk_Manager)
 
-
-### Advanced usage ( for Debug and Develop only )
-
-#### Enter manual mode
-
-If you want to control xray by running command totally, just add a file `/data/adb/xray/manual`.  In this situation, xray service won't start on boot automatically and you cann't manage service start/stop via Magisk Manager App. 
-
-
-#### Select which UID to proxy
-
-- If you expect transparent proxy ( read Transparent proxy section for more detail ) for specific Apps, just write down these Apps' uid in file `/data/adb/xray/appid.list` . 
+- If you expect transparent proxy ( read Transparent proxy section for more detail ) for specific Apps, just write down these Apps' uid in file `/data/adb/xray/appid.list` .
 
   Each App's uid should separate by space or just one App's uid per line. ( for Android App's uid , you can search App's package name in file `/data/system/packages.list` , or you can look into some App like Shadowsocks. )
 
 - If you expect all Apps proxy by xray with transparent proxy, just write `ALL` in file `/data/adb/xray/appid.list` .
 
-- If you expect all Apps proxy by xray with transparent proxy EXCEPT specific Apps, write down `bypass` at the first line then these Apps' uid separated as above in file `/data/adb/xray/appid.list`. 
+- If you expect all Apps proxy by xray with transparent proxy EXCEPT specific Apps, write down `bypass` at the first line then these Apps' uid separated as above in file `/data/adb/xray/appid.list`.
 
 - Transparent proxy won't take effect until the xray service start normally and file `/data/adb/xray/appid.list` is not empty.
 
+### Multi-Core Switching
+
+Modify the `#Settings` entry in `xray.config` in the `/data/adb/xray/` directory to determine the core and configuration files to use.
+
+```
+config=
+custom=
+```
+
+Valid values for `config` are the full configuration file name, xray/v2ray defaults to `config.json` and sing-box defaults to `sing-config.json`.
+
+`custom` is the core document to be used, with a valid value of `xray`/`v2ray`/`sing-box`
+
+### Advanced usage ( for Debug and Develop only )
+
+#### Enter manual mode
+
+If you want to control xray by running command totally, just add a file `/data/adb/xray/manual`. In this situation, xray service won't start on boot automatically and you cann't manage service start/stop via Magisk Manager App.
 
 #### Manage service start / stop
 
@@ -89,7 +118,7 @@ If you want to control xray by running command totally, just add a file `/data/a
 
 - For example, in my environment ( Magisk-alpha version: 23001 )
 
-  - Start service : 
+  - Start service :
 
     `/data/adb/xray/scripts/xray.service start`
 
@@ -97,14 +126,13 @@ If you want to control xray by running command totally, just add a file `/data/a
 
     `/data/adb/img/xray/scripts/xray.service stop`
 
-
 #### Manage transparent proxy enable / disable
 
 - Transparent proxy script is `/data/adb/xray/scripts/xray.tproxy`.
 
 - For example, in my environment ( Magisk-alpha version: 23001 )
 
-  - Enable Transparent proxy : 
+  - Enable Transparent proxy :
 
     `/data/adb/xray/scripts/xray.tproxy enable`
 
@@ -112,17 +140,15 @@ If you want to control xray by running command totally, just add a file `/data/a
 
     `/data/adb/xray/scripts/xray.tproxy disable`
 
-
 #### Bypass Transparent proxy when connecting to WLAN
-TODO
 
+TODO
 
 #### Select which App to proxy, and which App to second proxy
+
 TODO
 
-
 #### Enable IPv6
-For best compatibility, this module disable IPv6 by default.
 
 To enable IPv6 proxy, execute `touch /data/adb/xray/ipv6`
 
@@ -132,48 +158,45 @@ To enable local IPv6 out, edit `base.json`, find the first inbound with "freedom
 
 To enable proxy IPv6 out, edit `proxy.json`, change its `"domainStrategy"` as what you do in `base.json`.
 
-
-#### Use v2ray instead of xray
-Rename v2ray to xray then replace `/data/adb/xray/bin/xray` with it.
-
-
-
 ## Uninstall
 
 1. Uninstall the module via Magisk Manager App.
 2. You can clean xray data dir by running command `rm -rf /data/adb/xray && rm -rf /data/adb/service.d/xray4magisk_service.sh` .
 
-
-
 ## FAQ
+
 No such file or directory?
+
 > You might need Busybox for Android NDK
 
 Error calling service activity?
+
 > This module is designed to automatically turn on and off Flight mode, in order to clear DNS cache. However, this only work when SELinux is premissive. So just ignore this error message in `service.log`, and if you like, do turn on and off Flight mode manually.
 
 Why I need turn on WIFI hotspot otherwise I cannot connect to Internet?
 Why I cannot connect to proxy server while using **domain name**?
+
 > It is a DNS issue. You need add `"sockopt": { "domainStrategy": "UseIP" }` to your proxy's `"streamSettings"`. By the way, this fix needs correct dns and routing configuration. If you don't know how to do, I suggest use IP address instead of domain name. Or use a Xray binary compiled with CGO enabled. Reference: [#12](https://github.com/CerteKim/Xray4Magisk/issues/12)
 
 This module cause battery drain really quick.
+
 > It might be a DNS issue, check `/data/adb/xray/run/error.log`.
 
 GUI support?
+
 > Not done yet.
 
 Why not store config files in Internal Storage?
+
 > For privacy. Some apps may read your data, check [Storage Isolation](https://sr.rikka.app/guide/)
 
 ## Contact
-- [Telegram](https://t.me/AsteriskFactory)
 
+- [Telegram](https://t.me/AsteriskFactory)
 
 ## Project X
 
 Project X is a set of network tools that help you to build your own computer network. It secures your network connections and thus protects your privacy. See [Project X](https://github.com/XTLS/xray-core) for more information.
-
-
 
 ## License
 
--- customize.sh
+++ customize.sh
@@ -6,9 +6,9 @@ SKIPUNZIP=1
 ASH_STANDALONE=1
 
 module_path="/data/adb/xray"
-config_file="${module_path}/confs/${config}"
 
 # External Tools
+[ -f "$MODPATH/common/addon.tar.xz" ] && tar -xf $MODPATH/common/addon.tar.xz -C $MODPATH/common >&2
 chmod -R 0755 $MODPATH/common/addon/Volume-Key-Selector/tools
 
 chooseport_legacy() {
@@ -25,10 +25,10 @@ chooseport_legacy() {
     elif [ $sel -eq 41 ]; then
       return 1
     elif $error; then
-      abort "Volume key not detected!"
+      abort "- Volume key not detected!"
     else
       error=true
-      echo "Volume key not detected. Try again"
+      ui_print "- Volume key not detected. Try again"
     fi
   done
 }
@@ -48,17 +48,17 @@ chooseport() {
       elif ($(grep -q 'KEY_VOLUMEDOWN *DOWN' $TMPDIR/events)); then
         return 1
       fi
-      [ $count -gt 6 ] && break
+      [ $count -gt 10 ] && break
     done
     if $error; then
       # abort "Volume key not detected!"
-      echo "Volume key not detected. Trying keycheck method"
+      ui_print "- Volume key not detected. Trying keycheck method"
       export chooseport=chooseport_legacy VKSEL=chooseport_legacy
       chooseport_legacy $delay
       return $?
     else
       error=true
-      echo "Volume key not detected. Try again"
+      ui_print "- Volume key not detected. Try again"
     fi
   done
 }
@@ -70,7 +70,8 @@ whichCustom() {
   for i in $(ls -tr /sdcard/Download); do
     if [ $(echo $i | grep -e '^Xray-.*\.zip$') ]; then
       core="xray"
-      ui_print "---Custom core selected as xray---"
+      ui_print
+      ui_print "- Custom core selected as xray"
       if [ ! $BOOTMODE ]; then
         asset="customDat"
       fi
@@ -78,7 +79,8 @@ whichCustom() {
       download_path="/sdcard/Download/${download_file}"
     elif [ $(echo $i | grep -e '^v2ray-.*\.zip$') ]; then
       core="v2ray"
-      ui_print "---Custom core selected as v2ray---"
+      ui_print
+      ui_print "- Custom core selected as v2ray"
       if [ ! $BOOTMODE ]; then
         asset="customDat"
       fi
@@ -86,7 +88,8 @@ whichCustom() {
       download_path="/sdcard/Download/${download_file}"
     elif [ $(echo $i | grep -e '^sing-box-.*\.tar.gz$') ]; then
       core="sing-box"
-      ui_print "---Custom core selected as sing-box---"
+      ui_print
+      ui_print "- Custom core selected as sing-box"
       if [ ! $BOOTMODE ]; then
         asset="customDb"
       fi
@@ -95,37 +98,44 @@ whichCustom() {
     fi
   done
   if [ $core == "custom" ]; then
-    ui_print "---Custom core not found---"
-    abort
+    ui_print
+    abort "- Please put custom core in /sdcard/Download"
   fi
 }
 
 # select asset
 selasset() {
-  ui_print "---Please select your asset---"
+  ui_print
+  ui_print "- Please select your asset"
   if [ $core == "sing-box" ]; then
-    ui_print "VOL+ = default, VOL- = custom"
+    ui_print "* VOL+ = default, VOL- = custom *"
     if $VKSEL; then
       asset="db"
-      ui_print "---Asset selected as default---"
+      ui_print
+      ui_print "- Asset selected as default"
     else
       asset="customDb"
-      ui_print "---Asset selected as custom---"
+      ui_print
+      ui_print "- Asset selected as custom"
     fi
   else
-    ui_print "VOL+ = default, VOL- = loyalsoldier/custom"
+    ui_print "* VOL+ = default, VOL- = loyalsoldier/custom *"
     if $VKSEL; then
       asset="dat"
-      ui_print "---Asset selected as default---"
+      ui_print
+      ui_print "- Asset selected as default"
     else
-      ui_print "---Please select loyalsoldier or custom---"
-      ui_print "VOL+ = loyalsoldier, VOL- = custom"
+      ui_print
+      ui_print "- Please select loyalsoldier or custom"
+      ui_print "* VOL+ = loyalsoldier, VOL- = custom *"
       if $VKSEL; then
         asset="loyalsoldier"
-        ui_print "---Asset selected as loyalsoldier---"
+        ui_print
+        ui_print "- Asset selected as loyalsoldier"
       else
         asset="customDat"
-        ui_print "---Asset selected as custom---"
+        ui_print
+        ui_print "- Asset selected as custom"
       fi
     fi
   fi
@@ -136,63 +146,75 @@ selcore() {
   if [ ! $BOOTMODE ]; then
     core="custom"
     core0="custom"
-    ui_print "---Work in offline mode, will use custom installation---"
+    ui_print
+    ui_print "- Work in offline mode, will use custom core & asset"
   else
     [ -f /sdcard/xray.config ] && source /sdcard/xray.config
     [ -f $module_path/xray.config ] && source $module_path/xray.config
     if [ ! $core ]; then
-      ui_print "---Do you want a custom installation---"
-      ui_print "(If you don't know, please choose NO)"
-      ui_print "VOL+ = NO, VOL- = YES"
+      ui_print
+      ui_print "- Do you have a custom core"
+      ui_print "-(If you don't know, please choose NO)"
+      ui_print "* VOL+ = NO, VOL- = YES *"
       if $VKSEL; then
-        ui_print "---Please select your core---"
-        ui_print "VOL+ = xray/v2ray, VOL- = sing-box"
+        ui_print
+        ui_print "- Please select your core"
+        ui_print "* VOL+ = xray/v2ray, VOL- = sing-box *"
         if $VKSEL; then
-          ui_print "---Please select xray or v2ray---"
-          ui_print "VOL+ = xray, VOL- = v2ray"
+          ui_print
+          ui_print "- Please select xray or v2ray"
+          ui_print "* VOL+ = xray, VOL- = v2ray *"
           if $VKSEL; then
             core="xray"
             core0="xray"
-            ui_print "---Core selected as xray---"
+            ui_print
+            ui_print "- Core selected as xray"
             selasset
           else
-            ui_print "---Please select v2fly or sagernet---"
-            ui_print "VOL+ = v2fly, VOL- = sagernet"
+            ui_print
+            ui_print "- Please select v2fly or sagernet"
+            ui_print "* VOL+ = v2fly, VOL- = sagernet *"
             if $VKSEL; then
               core="v2ray"
               core0="v2ray"
-              ui_print "---Core selected as v2ray(v2fly)---"
+              ui_print
+              ui_print "- Core selected as v2ray(v2fly)"
               selasset
             else
               core="sagernet"
               core0="sagernet"
-              ui_print "---Core selected as v2ray(sagerNet)---"
+              ui_print
+              ui_print "- Core selected as v2ray(sagerNet)"
               selasset
             fi
           fi
         else
           core="sing-box"
           core0="sing-box"
-          ui_print "---Core selected as sing-box---"
+          ui_print
+          ui_print "- Core selected as sing-box"
           selasset
         fi
       else
         core="custom"
         core0="custom"
-        ui_print "---Start custom installation---"
+        ui_print
+        ui_print "- Start custom installation"
         selasset
       fi
     else
-      ui_print "---Core selected as $core---"
-      ui_print "---Asset selected as $asset---"
       core0=$core
+      ui_print
+      ui_print "- Core selected as $core"
+      ui_print
+      ui_print "- Asset selected as $asset"
     fi
   fi
 }
 
 # get version arg
 whichArch() {
-  ui_print "---Select arch---"
+  ui_print "- Select arch"
   case "${core}" in
   xray)
     case "${ARCH}" in
@@ -247,19 +269,17 @@ whichArch() {
 
 # deploy assets
 asset() {
-  ui_print "---Deploy assets---"
+  ui_print "- Deploy assets"
   mkdir -p ${module_path}/assets
   case "${asset}" in
   loyalsoldier)
     /data/adb/magisk/busybox wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat -O /sdcard/Download/geoip.dat >&2
     if [ "$?" != "0" ]; then
-      ui_print "---Download err---"
-      abort
+      abort "- Download err"
     fi
     /data/adb/magisk/busybox wget https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat -O /sdcard/Download/geosite.dat >&2
     if [ "$?" != "0" ]; then
-      ui_print "---Download err---"
-      abort
+      abort "- Download err"
     fi
     cp /sdcard/Download/geoip.dat ${module_path}/assets
     cp /sdcard/Download/geosite.dat ${module_path}/assets
@@ -271,13 +291,11 @@ asset() {
   db)
     /data/adb/magisk/busybox wget https://github.com/SagerNet/sing-geoip/releases/latest/download/geoip.db -O /sdcard/Download/geoip.db >&2
     if [ "$?" != "0" ]; then
-      ui_print "---Download err---"
-      abort
+      abort "- Download err"
     fi
     /data/adb/magisk/busybox wget https://github.com/SagerNet/sing-geosite/releases/latest/download/geosite.db -O /sdcard/Download/geosite.db >&2
     if [ "$?" != "0" ]; then
-      ui_print "---Download err---"
-      abort
+      abort "- Download err"
     fi
     cp /sdcard/Download/geoip.db ${module_path}/assets
     cp /sdcard/Download/geosite.db ${module_path}/assets
@@ -287,8 +305,7 @@ asset() {
       cp /sdcard/Download/geoip.dat ${module_path}/assets
       cp /sdcard/Download/geosite.dat ${module_path}/assets
     else
-      ui_print "---Please put geoip.dat & geosite.dat in /sdcard/Download---"
-      abort
+      abort "- Please put geoip.dat & geosite.dat in /sdcard/Download"
     fi
     ;;
   customDb)
@@ -296,8 +313,7 @@ asset() {
       cp /sdcard/Download/geoip.db ${module_path}/assets
       cp /sdcard/Download/geosite.db ${module_path}/assets
     else
-      ui_print "---Please put geoip.db & geosite.db in /sdcard/Download---"
-      abort
+      abort "- Please put geoip.db & geosite.db in /sdcard/Download"
     fi
     ;;
   esac
@@ -305,10 +321,11 @@ asset() {
 
 # get core
 whichCore() {
-  ui_print "---Download core---"
-  case "$core" in
+  ui_print
+  ui_print "- Search core"
+  case "${core}" in
   custom)
-    latest_version=custom
+    latest_version="custom"
     whichCustom
     ;;
   xray)
@@ -321,8 +338,7 @@ whichCore() {
     download_path="/sdcard/Download/${download_file}"
     /data/adb/magisk/busybox wget "${download_link}/download/v${latest_version}/${download_file}" -O "${download_path}" >&2
     if [ "$?" != "0" ]; then
-      ui_print "---Download err---"
-      abort
+      abort "- Download err"
     fi
     ;;
   v2ray)
@@ -335,8 +351,7 @@ whichCore() {
     download_path="/sdcard/Download/${download_file}"
     /data/adb/magisk/busybox wget "${download_link}/download/v${latest_version}/${download_file}" -O "${download_path}" >&2
     if [ "$?" != "0" ]; then
-      ui_print "---Download err---"
-      abort
+      abort "- Download err"
     fi
     ;;
   sagernet)
@@ -349,8 +364,7 @@ whichCore() {
     download_path="/sdcard/Download/${download_file}"
     /data/adb/magisk/busybox wget "${download_link}/download/v${latest_version}/${download_file}" -O "${download_path}" >&2
     if [ "$?" != "0" ]; then
-      ui_print "---Download err---"
-      abort
+      abort "- Download err"
     fi
     ;;
   sing-box)
@@ -366,8 +380,7 @@ whichCore() {
     download_path="/sdcard/Download/${download_file}"
     /data/adb/magisk/busybox wget "${download_link}/download/v${latest_version}/${download_file}" -O "${download_path}" >&2
     if [ "$?" != "0" ]; then
-      ui_print "---Download err---"
-      abort
+      abort "- Download err"
     fi
     ;;
   esac
@@ -375,23 +388,27 @@ whichCore() {
 
 # deploy core
 core() {
-  ui_print "---Deploy core---"
+  ui_print "- Deploy core"
   mkdir -p ${module_path}/bin
   case "${core}" in
   xray)
     unzip -j -o "${download_path}" "xray" -d ${module_path}/bin/ >&2
+    config="config.json"
     ;;
   v2ray | sagernet)
     unzip -j -o "${download_path}" "v2ray" -d ${module_path}/bin/ >&2
+    config="config.json"
     ;;
   sing-box)
     tar --strip-components=1 -xvzf ${download_path} -C ${module_path}/bin/ >&2
+    config="sing-config.json"
+    rm -f ${module_path}/bin/LICENSE
     ;;
   esac
 }
 
 installModule() {
-  ui_print "---Release scripts---"
+  ui_print "- Release scripts"
   mkdir -p ${module_path}/run
   mkdir -p ${module_path}/confs
   mkdir -p ${module_path}/scripts
@@ -403,10 +420,11 @@ installModule() {
   unzip -j -o "${ZIPFILE}" 'uninstall.sh' -d $MODPATH >&2
   set_perm ${module_path}/scripts/xray.service 0 0 0755
 
+  config_file="${module_path}/confs/${config}"
   [ -f ${module_path}/xray.config ] ||
     unzip -j -o "${ZIPFILE}" "xray/etc/xray.config" -d ${module_path} >&2
   [ -f ${config_file} ] ||
-    unzip -j -o "${ZIPFILE}" "xray/etc/confs/*" -d ${module_path}/confs >&2
+    unzip -j -o "${ZIPFILE}" "xray/etc/confs/${config}" -d ${module_path}/confs >&2
   [ -f ${module_path}/appid.list ] ||
     echo "ALL" >${module_path}/appid.list
   [ -f ${module_path}/ignore_out.list ] ||
@@ -424,11 +442,11 @@ installModule() {
   set_perm_recursive ${module_path}/bin 0 0 0755 0755
 
   unzip -j -o "${ZIPFILE}" "module.prop" -d $MODPATH >&2
-  echo -n "version=Module v2.1.1, Core " >>$MODPATH/module.prop
+  echo -n "version=Module v2.2, Core " >>$MODPATH/module.prop
   echo "${core} ${latest_version}" >>$MODPATH/module.prop
-  echo "versionCode=20220919" >>$MODPATH/module.prop
+  echo "versionCode=20220925" >>$MODPATH/module.prop
   echo "# Settings" >${module_path}/xray.config
-  echo "config="config.json"" >>${module_path}/xray.config
+  echo "config=$config" >>${module_path}/xray.config
   echo "custom=$core" >>${module_path}/xray.config
   echo "# Update" >>${module_path}/xray.config
   echo "core=$core0" >>${module_path}/xray.config
