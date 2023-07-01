0c38c9ce91a5057054ffd6bde683b056a89adea4
Whale Choi <cj1369636717@gmail.com>
2023-07-01 10:50:44 +0800

    always show xrayhelper intro on startup

--- xray/scripts/start.sh
+++ xray/scripts/start.sh
@@ -6,13 +6,12 @@ XRAYHELPER=/data/adb/xray/bin/xrayhelper
 XRAYHELPER_CONF=/data/adb/xray/xrayhelper.yml
 
 start_proxy() {
-    ${XRAYHELPER} &>>/data/adb/xray/run/helper.log
     ${XRAYHELPER} -c ${XRAYHELPER_CONF} service start &>>/data/adb/xray/run/helper.log &&
         ${XRAYHELPER} -c ${XRAYHELPER_CONF} proxy enable &>>/data/adb/xray/run/helper.log &
 }
 
 if [ ! -f /data/adb/xray/manual ]; then
-    echo -n "" >/data/adb/xray/run/helper.log
+    ${XRAYHELPER} &>/data/adb/xray/run/helper.log
     rm -rf /data/adb/xray/run/core.pid
     rm -rf /data/adb/xray/run/tun2socks.pid
     if [ ! -f ${MODDIR}/disable ]; then
