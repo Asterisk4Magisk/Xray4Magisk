1a5c008612fa38dff353257878c0397a7ddcbcf2
Whale Choi <cj1369636717@gmail.com>
2024-08-11 21:49:07 +0800

    update Module v3.4.5

--- changelog.md
+++ changelog.md
@@ -1,10 +1,13 @@
 ## Changelog
 ### Module
-- use xrayhelper v1.4.4
-- use xray4magisk-ksu-webui v0.0.3
+- use xrayhelper v1.4.5
+- use xray4magisk-ksu-webui v0.0.4
 
-### XrayHelper v1.4.4
-- [add allowInsecure option](https://github.com/Asterisk4Magisk/XrayHelper/commit/7410562b7fcf81b2c2ea95fea3bf2bb693bbf933)
+### XrayHelper v1.4.5
+- [fix xray httpupgrade/splithttp settings object host field](https://github.com/Asterisk4Magisk/XrayHelper/commit/e44fc8a754fd556347ca91e3828ec437811ff299)
+- [add cpuLimit and memLimit options](https://github.com/Asterisk4Magisk/XrayHelper/commit/574f34021f22a5ecdc50efd3432a3256574367cd)
+- [better way to bypass external ip](https://github.com/Asterisk4Magisk/XrayHelper/commit/df255732698b90f1b68ef87a25c92d1bcc124327)
+- [check service status before enable proxy](https://github.com/Asterisk4Magisk/XrayHelper/commit/28324757b8898487e1513de3e6a220d749ba08f9)
 
-### xray4magisk-ksu-webui v0.0.3
-- sync XrayHelper v1.4.4
+### xray4magisk-ksu-webui v0.0.4
+- [sync with xrayhelper 1.4.5](https://github.com/Asterisk4Magisk/xray4magisk-ksu-webui/commit/907462af18f1fb6dc3c9ecf57beeeb4737c2e22d)
--- xray/scripts/start.sh
+++ xray/scripts/start.sh
@@ -1,6 +1,7 @@
 #!/system/bin/sh
 
-MODDIR=/data/adb/modules/xray4magisk
+MOD_DIR=/data/adb/modules/xray4magisk
+NET_DIR=/data/misc/net
 SCRIPTS_DIR=/data/adb/xray/scripts
 XRAYHELPER=/data/adb/xray/bin/xrayhelper
 XRAYHELPER_CONF=/data/adb/xray/xrayhelper.yml
@@ -10,13 +11,15 @@ start_proxy() {
         ${XRAYHELPER} -c ${XRAYHELPER_CONF} proxy enable &>>/data/adb/xray/run/helper.log &
 }
 
+rm -rf /data/adb/xray/run/core.pid
+rm -rf /data/adb/xray/run/adghome.pid
+rm -rf /data/adb/xray/run/tun2socks.pid
 if [ ! -f /data/adb/xray/manual ]; then
-    ${XRAYHELPER} &>/data/adb/xray/run/helper.log
-    rm -rf /data/adb/xray/run/core.pid
-    rm -rf /data/adb/xray/run/adghome.pid
-    rm -rf /data/adb/xray/run/tun2socks.pid
-    if [ ! -f ${MODDIR}/disable ]; then
+    ${XRAYHELPER} >/data/adb/xray/run/helper.log
+    if [ ! -f ${MOD_DIR}/disable ]; then
         start_proxy
+        sleep 5
     fi
-    inotifyd ${SCRIPTS_DIR}/xray.inotify ${MODDIR} &>>/data/adb/xray/run/helper.log &
+    inotifyd ${SCRIPTS_DIR}/xray.inotify ${MOD_DIR} &>>/data/adb/xray/run/helper.log &
+    inotifyd ${SCRIPTS_DIR}/net.inotify ${NET_DIR} &>>/data/adb/xray/run/helper.log &
 fi
